cb2cdb7a46c5555d67c4a7a39c304d48b5f99bf7
Tetra Silverberg <tetra.silv@gmail.com>
2024-09-20 19:03:22 -0400

    Removed unused shader.

--- src/modules/sdlext/init.h
+++ src/modules/sdlext/init.h
@@ -12,13 +12,6 @@
 #define WINDOW_INIT_WIDTH 800
 #define WINDOW_INIT_HEIGHT 600
 
-const char *vertex_shader_default = \
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main(){\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-
 void sdlext_init(
     SDL_Window** window, 
     SDL_GLContext* context, 
